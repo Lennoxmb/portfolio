69b3d10a2d8aab0c6fc09a9f04c44274e465c3ad
Lennox Ballard <noellemb@gmail.com>
2023-07-13 08:49:49 -0500

    editing security

--- src/main/java/com/example/portfolio/config/SecurityConfig.java
+++ src/main/java/com/example/portfolio/config/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/profile", "/profile/**", "/battlegrounds", "/battlegrounds/edit-battle-title", "/battlegrounds/**", "/battlegrounds/complete-task", "/battleList", "/claim/badge", "/edit/profile").authenticated()
-                        .requestMatchers("/register", "/registration", "/login", "/home", "/", "/about", "/contact", "/send-email", "/welcome", "/monsterList", "/how-to", "/error").permitAll()
+                        .requestMatchers("/profile", "/profile/**", "/edit/profile").authenticated()
+                        .requestMatchers("/register", "/registration", "/login", "/home", "/", "/about", "/contact", "/send-email", "/welcome", "/error").permitAll()
                         .requestMatchers("/css/**", "/js/**", "/images/**", "/keys.js").permitAll()
                 )
                 .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/profile"))
