3c14d2081a15a5d7031aef04ea6742ff37ae191d
Lennox Ballard <noellemb@gmail.com>
2023-07-13 07:53:00 -0500

    adding security

--- src/main/java/com/example/portfolio/config/SecurityConfig.java
+++ src/main/java/com/example/portfolio/config/SecurityConfig.java
@@ -1,46 +1,42 @@
 package com.example.portfolio.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@Import(CorsConfig.class)
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-
-//    -------------------UNCOMMENT THIS CODE FOR SECURED ACCESS ---------------------------
-//                        .requestMatchers("/tasks/create", "/tasks/*/edit", "/profile", "/battlegrounds").authenticated()
-//                        .requestMatchers("/tasks", "/templates/**", "/register", "/login", "/home", "/", "/contact", "/send-email").permitAll()
-//                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-//                )
-//                --------------------------------------------------------------
-//
-//    -------------------UNCOMMENT THIS CODE FOR ALL ACCESS -  NO SECURITY----------------
-                                .anyRequest().permitAll()
+        return http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/profile", "/profile/**", "/battlegrounds", "/battlegrounds/edit-battle-title", "/battlegrounds/**", "/battlegrounds/complete-task", "/battleList", "/claim/badge", "/edit/profile").authenticated()
+                        .requestMatchers("/register", "/registration", "/login", "/home", "/", "/about", "/contact", "/send-email", "/welcome", "/monsterList", "/how-to", "/error").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/keys.js").permitAll()
                 )
-//                --------------------------------------------------------------
-
-                .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/home"))
+                .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/profile"))
                 .logout((logout) -> logout.logoutSuccessUrl("/home"))
-                .httpBasic(withDefaults());
-        return http.build();
-
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Bean
+    public CookieCsrfTokenRepository csrfTokenRepository() {
+        return CookieCsrfTokenRepository.withHttpOnlyFalse();
+    }
 }
 
