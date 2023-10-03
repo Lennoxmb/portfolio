3567bd508f44d83bdd1918b0f4b987c26c9f8e8d
Lennox Ballard <noellemb@gmail.com>
2023-10-03 17:28:55 -0500

    changing navbar

--- src/main/java/com/example/portfolio/services/UserDetailsServices.java
+++ src/main/java/com/example/portfolio/services/UserDetailsServices.java
@@ -1,30 +1,30 @@
-package com.example.portfolio.services;
-
-
-import com.example.portfolio.models.User;
-import com.example.portfolio.models.UserDetail;
-import com.example.portfolio.repositories.UserRepo;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserDetailsServices implements UserDetailsService {
-    public final UserRepo usersDao;
-
-    public UserDetailsServices(UserRepo usersDao){
-        this.usersDao = usersDao;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = usersDao.findUserByUsername(username);
-        if (user == null){
-            throw new UsernameNotFoundException("User details not found for user: " + username);
-        } else {
-            return new UserDetail(user.getId(), user.getUsername(), user.getEmail(), user.getPassword());
-        }
-    }
-}
-
+//package com.example.portfolio.services;
+//
+//
+//import com.example.portfolio.models.User;
+//import com.example.portfolio.models.UserDetail;
+//import com.example.portfolio.repositories.UserRepo;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class UserDetailsServices implements UserDetailsService {
+//    public final UserRepo usersDao;
+//
+//    public UserDetailsServices(UserRepo usersDao){
+//        this.usersDao = usersDao;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = usersDao.findUserByUsername(username);
+//        if (user == null){
+//            throw new UsernameNotFoundException("User details not found for user: " + username);
+//        } else {
+//            return new UserDetail(user.getId(), user.getUsername(), user.getEmail(), user.getPassword());
+//        }
+//    }
+//}
+//
